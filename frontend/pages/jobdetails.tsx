df02e433f1234ba13beb0f9862a87aab097aeeb7
Berivan Kürekci <berivan.kuerekci@students.unibe.ch>
2018-10-03 15:46:46 +0200

    WIP jobdetails forms

--- frontend/pages/jobdetails.tsx
+++ frontend/pages/jobdetails.tsx
@@ -1,8 +1,50 @@
 import * as React from "react";
 import "semantic-ui-css/semantic.min.css";
+import { Button, Checkbox, Form } from "semantic-ui-react";
 
-export default () => (
-  <div>
-    <h1>Job Details</h1>
-  </div>
+interface Job {
+  id: number;
+  title: string;
+  description: string;
+  skills: string;
+  salary: number;
+  schedule: string;
+  period: string;
+  employer: string;
+}
+
+const FormExampleForm = () => (
+  <Form>
+    <Form.Field>
+      <label>title</label>
+      <input placeholder="title" />
+    </Form.Field>
+    <Form.Field>
+      <label>description</label>
+      <input placeholder="description" />
+    </Form.Field>
+    <Form.Field>
+      <label>skills</label>
+      <input placeholder="skills" />
+    </Form.Field>
+    <Form.Field>
+      <label>salary</label>
+      <input placeholder="salary" />
+    </Form.Field>
+    <Form.Field>
+      <label>schedule</label>
+      <input placeholder="schedule" />
+    </Form.Field>
+    <Form.Field>
+      <label>period</label>
+      <input placeholder="period" />
+    </Form.Field>
+    <Form.Field>
+      <label>employer</label>
+      <input placeholder="employer" />
+    </Form.Field>
+    <Button type="submit">Submit</Button>
+  </Form>
 );
+
+export default FormExampleForm;
