d65a344c323c1211ee1115dfb54dc7b0316fa64a
Fabio Bertagna <flbertagna@gmail.com>
2018-10-03 09:05:41 +0200

    Added main components for the JobList view

---
+++ frontend/components/joblist/JobItem.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { Header } from "semantic-ui-react";
+
+export default ({ children }) => <Header as={"h3"}>{children}</Header>;
--- frontend/pages/joblist.tsx
+++ frontend/pages/joblist.tsx
@@ -1,8 +1,49 @@
-import * as React from "react";
-import "semantic-ui-css/semantic.min.css";
+import React from "react";
+import JobList from "../components/joblist/JobList";
+import JobItem from "../components/joblist/JobItem";
+import { Container } from "semantic-ui-react";
+
+interface Job {
+  id: string;
+  title: string;
+  description: string;
+  skills: string[];
+  salary: number;
+  schedule: string[];
+  period: string;
+  employer: string;
+}
+
+const joblist: Array<Job> = [
+  {
+    id: "c41426d7-219e-4353-b38f-a7d6b2e5dac5",
+    title: "Frontend Developer",
+    description: "Part time Junior frontend developer",
+    skills: ["HTML", "CSS", "JavaScript or TypeScript", "NextJS"],
+    salary: 24.5,
+    schedule: ["Monday morning", "Tuesday morning"],
+    period: "undefined",
+    employer: "BWL GmbH"
+  },
+  {
+    id: "0e12a329-66fd-4669-9420-c04520e372a4",
+    title: "Backend Developer",
+    description: "Part time Junior backend developer",
+    skills: ["JavaScript or TypeScript", "GraphQL"],
+    salary: 25.5,
+    schedule: ["Thursday morning", "Friday morning"],
+    period: "undefined",
+    employer: "BWL GmbH"
+  }
+];
 
 export default () => (
-  <div>
-    <h1>Job List</h1>
-  </div>
+  <Container text>
+    <h1>Job list</h1>
+    <JobList>
+      {joblist.map(job => (
+        <JobItem>{job.title}</JobItem>
+      ))}
+    </JobList>
+  </Container>
 );
