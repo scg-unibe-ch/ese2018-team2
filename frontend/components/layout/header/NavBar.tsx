7b1595da49aaa8f1e291592eb1630b7a62b6a2f8
Fabio Bertagna <flbertagna@gmail.com>
2018-10-05 08:56:25 +0200

    Fixed routing of the NavBar

--- frontend/components/layout/header/NavBar.tsx
+++ frontend/components/layout/header/NavBar.tsx
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
+import Router from "next/router";
 
 export default class NavBar extends Component {
   state = {};
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name });
+    Router.push(this.getPageName(name));
+  };
 
+  getPageName(name) {
+    if (name === "home") {
+      return "/index";
+    }
+    if (name === "joblist") {
+      return "/joblist";
+    }
+    if (name === "jobdetails") {
+      return "/jobdetails";
+    } else {
+      return "#";
+    }
+  }
   render() {
     const activeItem = this.state;
 
     return (
-      <Menu fluid widths={3}>
+      <Menu fluid widths={6}>
         <Menu.Item
           name="home"
           active={activeItem === "home"}
