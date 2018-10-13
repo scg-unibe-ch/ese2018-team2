a881f23807cb1a79db049ddfec91646565df0d98
Fabio Bertagna <flbertagna@gmail.com>
2018-10-13 17:45:30 +0200

    Initialised custom server

---
+++ frontend/lib/routes.ts
@@ -0,0 +1,20 @@
+const nextRoutes = require("next-routes");
+const routes = nextRoutes();
+
+routes.matchPath = path => {
+  const result = routes.match(path);
+  if (result.route && result.route.name && result.params) {
+    return {
+      route: result.route.name,
+      params: result.params
+    };
+  }
+  return null;
+};
+
+routes
+  .add("index", "/")
+  .add("jobs", "/jobs")
+  .add("jobdetails", "/jobdetails");
+
+module.exports = routes;
