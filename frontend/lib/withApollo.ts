509eb714c97132c091efc0b909f12e7010a775e1
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-10-30 17:36:49 +0100

    Use include to send cookie to back end

--- frontend/lib/withApollo.ts
+++ frontend/lib/withApollo.ts
@@ -1,6 +1,10 @@
 import ApolloClient from "apollo-boost";
 import withApollo from "next-with-apollo/lib/withApollo";
 
-export default withApollo(() => (
-  new ApolloClient({ uri: "http://localhost:4000" })
-))
+export default withApollo(
+  () =>
+    new ApolloClient({
+      uri: "http://localhost:4000",
+      credentials: "include"
+    })
+);
