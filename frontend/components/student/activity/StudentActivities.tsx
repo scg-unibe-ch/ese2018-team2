5722778d4766929cbacbc8a476db010fc13e5f25
Fabio Bertagna <flbertagna@gmail.com>
2018-11-02 14:54:47 +0100

    Init Activity elements

--- frontend/components/student/activity/StudentActivities.tsx
+++ frontend/components/student/activity/StudentActivities.tsx
@@ -1,23 +1,91 @@
 import {SingletonRouter} from "next/router";
 import * as React from "react";
-import {Header, Segment} from "semantic-ui-react";
-import withAuthorization from "../../Auth/withAuthorization";
-import { ApolloError } from "apollo-boost";
+import {Card, Header, Loader, Segment} from "semantic-ui-react";
+import {ApolloError} from "apollo-boost";
+import gql from "graphql-tag";
+import {Query} from "react-apollo";
+import StudentActivity from "./StudentActivity";
+import StudentJobCard from "../StudentJobCard";
+
+
+interface ActivitiesJobs {
+    jobs: {
+        id: string;
+        title: string;
+        description: string;
+        organization: { id: string; name };
+    }[];
+}
+
 
 interface StudentActivitiesProps {
     router?: SingletonRouter;
     loading: boolean;
     error: ApolloError;
-    data: AllJobs;
-
+    data: ActivitiesJobs;
 }
 
-const StudentActivities: React.SFC<StudentActivitiesProps> = () => {
+export const GET_ACTIVITIES_JOBS = gql`
+query StudentActivities {
+    jobs {
+      id
+      title
+      description
+      organization {
+        id
+        name
+      }
+    }
+  }
+`;
+
+
+const StudentActivities: React.SFC<StudentActivitiesProps> = ({router, loading, error, data}) => {
+
+    if (loading) {
+        return (
+            <Segment>
+                <Header as="h3">Recommended Jobs</Header>
+                <Card.Group itemsPerRow={3} centered>
+                    <Card><Loader/></Card>
+                    <Card><Loader/></Card>
+                    <Card><Loader/></Card>
+                </Card.Group>
+            </Segment>
+        )
+    }
+
+    if (error) {
+        return (
+            <Segment>
+                <Header as="h3">Recommended Jobs</Header>
+                <Card.Group itemsPerRow={3} centered>
+                    <Card.Header>Ooops</Card.Header>
+                    <Card><Header as="h3">Ooops</Header></Card>
+                    <Card><Header as="h3">Ooops</Header></Card>
+                </Card.Group>
+            </Segment>
+        )
+    }
+
+
     return (
         <Segment>
             <Header>My Activities</Header>
+            <Card.Group itemsPerRow={3} centered>
+                {data.jobs.map(job => (
+                    <StudentActivity key={job.id} job={job}/>
+                ))}
+            </Card.Group>
         </Segment>
     );
 };
 
-export default withAuthorization(StudentActivities);
+
+export default () => (
+    <Query query={GET_ACTIVITIES_JOBS}>
+        {({loading, error, data}) => (
+            <StudentActivities data={data} error={error} loading={loading}/>
+        )}
+    </Query>
+);
