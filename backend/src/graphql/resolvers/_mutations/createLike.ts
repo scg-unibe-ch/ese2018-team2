fd979b81ace55fce50a57e2de069edaad3542145
Fabio Bertagna <flbertagna@gmail.com>
2018-11-04 02:56:43 +0100

    WIP added myLikes. Query not working yet

--- backend/src/graphql/resolvers/_mutations/createLike.ts
+++
@@ -1,7 +0,0 @@
-import { MutationResolvers } from "../../__generated__/graphqlgen";
-
-const createLike: MutationResolvers.CreateLikeResolver = (_, args, ctx) => {
-  return ctx.likeRepository.createLike(args.jobId, ctx.session);
-};
-
-export default createLike;
--- backend/src/graphql/resolvers/_mutations/deleteLike.ts
+++
@@ -1,7 +0,0 @@
-import { MutationResolvers } from "../../__generated__/graphqlgen";
-
-const deleteLike: MutationResolvers.DeleteLikeResolver = (_, args, ctx) => {
-  return ctx.likeRepository.deleteLike(args.jobId, ctx.session);
-};
-
-export default deleteLike;
