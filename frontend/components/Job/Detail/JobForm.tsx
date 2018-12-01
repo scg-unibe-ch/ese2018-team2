cba962a5c8d7f78723ac5c08cfc71d4ebbebc98c
Fabio Bertagna <flbertagna@gmail.com>
2018-12-01 14:36:53 +0100

    Cleaned up deprecated Components

--- frontend/components/Job/Detail/JobForm.tsx
+++ frontend/components/Job/Detail/JobForm.tsx
@@ -3,7 +3,7 @@ import { Form, Button, Confirm } from "semantic-ui-react";
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 import JobDeleteButton from "./JobDeleteButton";
-import ApplyButton from "../../student/ApplyButton";
+import ApplyButton from "../../Student/ApplyButton";
 
 interface JobFormProps {
   data: {
--- frontend/components/layout/footer/PageFooter.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import "semantic-ui-css/semantic.min.css";
-import { Header } from "semantic-ui-react";
-
-export default () => (
-  <div>
-    <Header as={"h5"}>I'm the Footer</Header>
-  </div>
-);
