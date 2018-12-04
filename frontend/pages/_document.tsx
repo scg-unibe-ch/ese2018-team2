817ec7fa03fddc16ecbf55572f9d65377481cdb8
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-12-04 16:16:52 +0100

    Fix main.css not found

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,12 +1,12 @@
 import Document, { Head, Main, NextScript } from "next/document";
 import * as React from "react";
+import "semantic-ui-css/semantic.min.css";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <html>
         <Head>
-          <link rel="stylesheet" href="/_next/static/style.css" />
           <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
         </Head>
         <body>
