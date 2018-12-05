0e2ef6a26ba92ed7fae6de0637c6cc5878f84284
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-12-05 15:58:47 +0100

    Show cards on new page

--- frontend/components/Search/SearchPage/Page.tsx
+++ frontend/components/Search/SearchPage/Page.tsx
@@ -1,9 +1,68 @@
+import gql from "graphql-tag";
+import { withRouter, WithRouterProps } from "next/router";
 import * as React from "react";
+import { Query } from "react-apollo";
 import "semantic-ui-css/semantic.min.css";
-import { Container, Segment } from "semantic-ui-react";
+import { Card, Container, Segment } from "semantic-ui-react";
 import { withIntialMe } from "../../../lib/withMe";
 import NavBar from "../../Frame/NavBar";
 import SearchField from "../SearchField";
+import Link from "next/link";
+
+const query = gql`
+  query JobSearch($search: String) {
+    search(search: $search) {
+      nodes {
+        id
+        title
+        description
+        organization {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+const SearchSegmentComponent: React.SFC<WithRouterProps> = ({ router }) => (
+  <Query
+    query={query}
+    variables={{ search: router.query && router.query.search }}
+  >
+    {({ loading, data }) => (
+      <Segment loading={loading} vertical>
+        <Container>
+          <Card.Group stackable itemsPerRow={4}>
+            {!data.loading &&
+              data.search &&
+              data.search.nodes &&
+              (data.search.nodes as {
+                id: string;
+                title: string;
+                description: string;
+              }[]).map(job => (
+                <Link
+                  key={job.id}
+                  href={{ pathname: "/job", query: { id: job.id } }}
+                  passHref
+                >
+                  <Card link>
+                    <Card.Content>
+                      <Card.Header>{job.title}</Card.Header>
+                      <Card.Description>{job.description}</Card.Description>
+                    </Card.Content>
+                  </Card>
+                </Link>
+              ))}
+          </Card.Group>
+        </Container>
+      </Segment>
+    )}
+  </Query>
+);
+
+const SearchSegment = withRouter(SearchSegmentComponent);
 
 export default withIntialMe(() => (
   <React.Fragment>
@@ -13,10 +72,6 @@ export default withIntialMe(() => (
         <SearchField />
       </Container>
     </Segment>
-    <Segment vertical>
-      <Container>
-        <p>Content</p>
-      </Container>
-    </Segment>
+    <SearchSegment />
   </React.Fragment>
 ));
