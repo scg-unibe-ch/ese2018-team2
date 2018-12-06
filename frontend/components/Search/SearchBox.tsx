34345082771646d8ffd9072ccca591893978cde7
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-12-06 12:17:31 +0100

    Fast page switch with prefetch

--- frontend/components/Search/SearchBox.tsx
+++ frontend/components/Search/SearchBox.tsx
@@ -1,23 +1,49 @@
+import { withRouter, WithRouterProps } from "next/router";
 import * as React from "react";
-import { Segment, Header, Form, Button } from "semantic-ui-react";
+import { Button, Form, Header, Segment } from "semantic-ui-react";
 
-const SearchBoxComponent = () => (
-  <Segment>
-    <Header as={"h2"}>Suche nach einem Job.</Header>
-    <Form action={"/s"} method={"GET"}>
-      <Form.Field>
-        <label>WAS?</label>
-        <input
-          type={"text"}
-          placeholder={"Informatik, Lektorat..."}
-          name={"search"}
-        />
-      </Form.Field>
-      <Button type="submit" color={"green"}>
-        Suche
-      </Button>
-    </Form>
-  </Segment>
-);
+class SearchBoxComponent extends React.Component<WithRouterProps> {
+  componentDidMount() {
+    const { router } = this.props;
+    router.prefetch("/s");
+  }
 
-export default SearchBoxComponent;
+  render() {
+    const { router } = this.props;
+
+    return (
+      <Segment>
+        <Header as={"h2"}>Suche nach einem Job.</Header>
+        <Form
+          action={"/s"}
+          method={"GET"}
+          onSubmit={async e => {
+            e.preventDefault();
+            //@ts-ignore
+            const data = new FormData(e.target);
+            await router.push({
+              pathname: "/s",
+              query: {
+                search: data.get("search")
+              }
+            });
+          }}
+        >
+          <Form.Field>
+            <label>WAS?</label>
+            <input
+              type={"text"}
+              placeholder={"Informatik, Lektorat..."}
+              name={"search"}
+            />
+          </Form.Field>
+          <Button type="submit" color={"green"}>
+            Suche
+          </Button>
+        </Form>
+      </Segment>
+    );
+  }
+}
+
+export default withRouter(SearchBoxComponent);
