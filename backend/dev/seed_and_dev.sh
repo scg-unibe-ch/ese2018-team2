33b9ca082279051ffd467ac0890085a3a201ca02
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-12-06 15:12:23 +0100

    Update postgres and use psql from container

--- backend/dev/seed_and_dev.sh
+++ backend/dev/seed_and_dev.sh
@@ -7,8 +7,7 @@ docker-compose up -d;
 
 # https://stackoverflow.com/questions/35069027/docker-wait-for-postgresql-to-be-running
 RETRIES=10
-
-until psql -H postgres://postgres@localhost:5432/postgres -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until docker run -it --rm --network=backend_default --link postgres postgres:11.1-alpine psql -H postgres://postgres@postgres:5432 -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 1
 done
